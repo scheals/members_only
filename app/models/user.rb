a22182ee6f47e98b24ab60b4bb3d4c8c0c50ae74
Scheals <95084627+scheals@users.noreply.github.com>
2022-11-11 08:18:11 +0100

    feat: add User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates :name, uniqueness: true, presence: true, length: { minimum: 3 }
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true, length: { minimum: 8 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
