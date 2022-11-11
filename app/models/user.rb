2616e02131e383a0d3088794df47bcaf91949923
Scheals <95084627+scheals@users.noreply.github.com>
2022-11-11 08:39:27 +0100

    feat: add username updating

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   validates :name, uniqueness: true, presence: true, length: { minimum: 3 }
   validates :email, uniqueness: true, presence: true
-  validates :password, presence: true, length: { minimum: 8 }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
