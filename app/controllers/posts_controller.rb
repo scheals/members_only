0fbc47553ba5ac3216e78b21340b63d56a546620
Scheals <95084627+scheals@users.noreply.github.com>
2022-11-11 09:40:08 +0100

    feat: add creating secret posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,26 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_url
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body).merge({ user_id: current_user.id })
   end
 end
